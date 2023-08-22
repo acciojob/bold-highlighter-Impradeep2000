190d510781eaf1f68f9dc4813179ca9dc4b54314
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-22 19:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
-
-
+	var boldTags = document.getElementsByTagName("strong");
+    for (var i = 0; i < boldTags.length; i++) {
+        boldTags[i].style.color = "green";
+    }
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	var boldTags = document.getElementsByTagName("strong");
+    for (var i = 0; i < boldTags.length; i++) {
+        boldTags[i].style.color = "black";
+    }
 }
